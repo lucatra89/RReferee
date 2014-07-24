f61456c095910b69f334a00954763bf0098b0db2
unil3o <coluichepuo@gmail.com>
2014-07-24 18:32:00 +0200

    terminata vista resocontoInfoView

--- js/router.js
+++ js/router.js
@@ -32,8 +32,8 @@ define(function(require) {
       "home": "home",
       "creazione": "creazione",
       "visio/:id": "visio",
-      "resocontoInfo/:id":"resocontoInfo",//per provare la pagina togliere il /:id dalla routes
-      "resocontoDati/:id" :"resocontoDati",   
+      "resocontoInfo":"resocontoInfo",//per provare la pagina togliere il /:id dalla routes
+      "resocontoDati/:id" :"resocontoDati",//per provare la pagina togliere il /:id dalla routes
     },
 
     initialize: function(options) {
@@ -100,7 +100,30 @@ define(function(require) {
     },
 
     resocontoInfo : function(id){
-      var model = this.matches.getMatchById(id);
+      // var model = this.matches.getMatchById(id);
+
+      var model = new Match({
+        'locali':'sulmona',
+        'ospiti':'teramo',
+        'aa1Giuste':'1',
+        'aa1Sbagliate':'2',
+        'aa1Giuste2':'3',
+        'aa1Sbagliate2':'4',
+        'aa2Giuste':'5',
+        'aa2Sbagliate':'6',
+        'aa2Giuste2':'7',
+        'aa2Sbagliate2':'8',
+        'barella1':'40',
+        'barella2':'20',
+        'inizio':'17:00',
+        'fine':'17:48',
+        'RecuperoSegnalato1T':'2',
+        'RecuperoEffettivo1T':'3',
+        'inizio2':'18:10',
+        'fine2':'18:55',
+        'RecuperoSegnalato2T':'1',
+        'RecuperoEffettivo2T':'2',
+      });
       var page = new resocontoInfo({model: model});
       
       this.changePage(page);
@@ -117,7 +140,7 @@ define(function(require) {
       }
       // go to first view
 
-      this.navigate("creazione" , {trigger: true});
+      this.navigate("resocontoInfo" , {trigger: true});
 
 
     },
